25f6ef09a4f4eb351c308bc4d98c2a8973eeaf00
Peko Roganovic <peko.roganovic@gmail.com>
2023-06-01 13:07:59 +0200

    proba mts cloud be

--- src/app/providers/global-vars.service.ts
+++ src/app/providers/global-vars.service.ts
@@ -5,14 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalVarsService {
 
-   baseURL = "https://sgbe21.herokuapp.com"
-   //baseURL = "http://localhost:8081";
+   //baseURL = "https://sgbe21.herokuapp.com"
+   //baseURL = "http://localhost:8080/sgis-2.0.1";
+   baseURL = "http://93.87.76.62:8080/sgis-2.0.1";
 
 
    constructor() {
    }
  
-   public setBaseURL(value: string) {
+   public setBaseURL(value: string): void {
      this.baseURL = value;
    }
  

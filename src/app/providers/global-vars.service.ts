8572eec59717bef527e3423056c7be093ab1d2f3
Peko Roganovic <peko.roganovic@gmail.com>
2023-06-08 14:20:20 +0200

    env

--- src/app/providers/global-vars.service.ts
+++ src/app/providers/global-vars.service.ts
@@ -5,9 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalVarsService {
 
-   baseURL = "https://sgbe21.herokuapp.com"
    //baseURL = "http://localhost:8080/sgis-2.0.1";
-   //baseURL = "https://93.87.76.62:8443/sgis-2.0.1";
+   baseURL = process.env['BCK_URL'] || '';
 
 
    constructor() {
